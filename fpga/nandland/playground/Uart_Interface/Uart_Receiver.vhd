90450d962234bfbbbf883bb46c2b8302c25ae395
Jitan Sahni <reachjitan@gmail.com>
2022-02-24 16:46:42 -0500

    sender UART

--- fpga/nandland/playground/Uart_Interface/Uart_Receiver.vhd
+++ fpga/nandland/playground/Uart_Interface/Uart_Receiver.vhd
@@ -10,6 +10,9 @@ entity Uart_Receiver is
         -- input wire that gives us bits 
         i_UART_RX     : in std_logic;
 
+        -- output wire that gives us bits 
+        o_UART_TX     : out std_logic;
+
          -- LED dislay - Segment1 is higher digit, Segment2 is lower digit
         o_Segment1_A  : out std_logic;
         o_Segment1_B  : out std_logic;
@@ -32,6 +35,8 @@ end entity Uart_Receiver;
 architecture RTL of Uart_Receiver is
     signal r_byte_read     : std_logic_vector(7 downto 0);
     signal r_byte_read_ready : std_logic := '0'; 
+    signal r_Uart_Tx_Active : std_logic := '0'; 
+    signal r_UART_TX     : std_logic;
 
     signal w_Segment1_A, w_Segment2_A : std_logic;
     signal w_Segment1_B, w_Segment2_B : std_logic;
@@ -49,6 +54,16 @@ begin
             i_Uart_Serial_Rx    => i_UART_RX,
             o_Byte_Read => r_byte_read,
             o_Byte_Ready => r_byte_read_ready
+    );
+
+    -- instantiate UART sender
+    Uart_Tx_Inst : entity work.Uart_Tx
+        port map (
+            i_Clk        => i_Clk,
+            o_Uart_Serial_Tx    => r_UART_TX,
+            o_Uart_Tx_Active => r_Uart_Tx_Active,
+            i_Byte_To_Send => r_byte_read,
+            i_Byte_Ready_To_send => r_byte_read_ready
     );
 
     -- Instantiate Binary to 7-Segment Converter
@@ -95,4 +110,8 @@ begin
     o_Segment1_E <= not w_Segment1_E;
     o_Segment1_F <= not w_Segment1_F;
     o_Segment1_G <= not w_Segment1_G;
+
+    -- send output only when UART TX is active
+    o_UART_TX <= r_UART_TX   when r_Uart_Tx_Active = '1'  else '1';
+    
 end architecture RTL;    
